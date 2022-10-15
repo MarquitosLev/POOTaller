b839d4f7dbaf2972768ebc2f7979dc3a65dfda00
gosti8 <sebastiandanielgosti8@gmail.com>
2022-10-15 02:11:27 -0300

    Ingreso y Lectura de Usuarios lista

--- TallerDePOO/src/main/java/Biblioteca/listaFuncionario.java
+++ TallerDePOO/src/main/java/Biblioteca/listaFuncionario.java
@@ -7,23 +7,32 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 import BibliotecaGraficos.ventanaRegistro;
 
 public class listaFuncionario {
-	ArrayList<Funcionario> datosFunc = new ArrayList<Funcionario>();
-	File functxt= new File ("Funcionarios.txt");
-	
+	ArrayList<Funcionario> datosFunc;
 	public void agregar(Funcionario obj) {
 		guardarFuncionario(obj);
-		datosFunc.add(obj);
 	}
 	
 	public boolean existe(String user) {
-		
+		datosFunc = new ArrayList<Funcionario>();
+		try {
+			FileReader fr = new FileReader("Funcionarios.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String lector;
+			while ((lector=br.readLine()) !=null) {			//Lee el archivo hasta el siguiente salto de linea
+				StringTokenizer X = new StringTokenizer(lector, "-");  //Se crea un String hasta que aparezca el "-"
+				datosFunc.add(new Funcionario(X.nextToken(), X.nextToken()));   
+			}
+		} catch (Exception e) {
+		}
 		if(datosFunc.size() == 0) return false;
-		for(int i = 0; i < datosFunc.size(); i++) {
-			if(this.datosFunc.get(i).getUsuario().equals(user)){
+		for(int i = 0; i < datosFunc.size(); i++) {		//Recorre el ArrayList hasta encontrar alguna coincidencia
+			if(datosFunc.get(i).getUsuario().equals(user)){
 				return true;
 			}
 		}
@@ -42,33 +51,17 @@ public class listaFuncionario {
 	
 	public void guardarFuncionario(Funcionario funcionario){
 		try {
+			File functxt = new File("Funcionarios.txt");
 			if (!functxt.exists()) {	//Crea el archivo txt en caso de que no exista
 				functxt.createNewFile();
             }
-			BufferedWriter lector = new BufferedWriter(new PrintWriter(functxt)); //Escribe los datos asignados
-			lector.write(" - " + funcionario.getUsuario());
-			lector.write(" - " + funcionario.getContrasenia());
-			lector.close();
+			FileWriter fw= new FileWriter ("Funcionarios.txt", true);
+			BufferedWriter br = new BufferedWriter(fw); //Escribe los datos asignados
+			PrintWriter escribir = new PrintWriter(br);
+			escribir.write(funcionario.getUsuario()+"-");
+			escribir.write(funcionario.getContrasenia() + "-"+"\n");
+			escribir.close();
 		} catch (Exception e) {
 		}
-		//mostrararchivos();
 	}
-	/*public static void mostrararchivos() {
-		try
-        {
-            FileReader fr=new FileReader("Funcionarios.txt");
-            BufferedReader br=new BufferedReader(fr);
-            String cadena;
-            while((cadena=br.readLine())!=null) //cuando en la siguiente linea leída no haya nada significa que terminó de leer los datos del archivo
-            {
-                System.out.println(""+cadena); 
-            }
-        }
-        catch(Exception ex)
-        {
-            System.out.println(ex.getMessage());
-        }
-		}
-	*/
-	
 }
